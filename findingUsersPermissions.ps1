a39bc631669daa6a787394774555df9740a7548d
Saeed Ashoor <91031639+Sashoor@users.noreply.github.com>
2023-02-23 12:18:39 +0000

    Add files via upload

--- findingUsersPermissions.ps1
+++ findingUsersPermissions.ps1
@@ -1,19 +1,36 @@
-# Authenticate to Azure and select all subscriptions
+# Login to Azure
 Connect-AzAccount
-$subIds = Get-AzSubscription | Select-Object -ExpandProperty SubscriptionId
 
-# Loop through all subscriptions and find user permissions
-foreach ($subId in $subIds) {
-  Set-AzContext -SubscriptionId $subId
-  $roles = Get-AzRoleDefinition | Select-Object -ExpandProperty Name
-  foreach ($role in $roles) {
-    $roleAssignments = Get-AzRoleAssignment -RoleDefinitionName $role
-    foreach ($assignment in $roleAssignments) {
-      Write-Output "Subscription ID: $($assignment.Scope.Split('/')[2])"
-      Write-Output "Role: $($role)"
-      Write-Output "User or Group: $($assignment.DisplayName)"
-      Write-Output "Object ID: $($assignment.ObjectId)"
-      Write-Output ""
+# Get all Azure subscriptions
+$subscriptions = Get-AzSubscription
+
+# Loop through each subscription
+foreach ($subscription in $subscriptions) {
+  # Set the current subscription context
+  Set-AzContext -SubscriptionId $subscription.Id
+
+  # Get all role assignments
+  $roleAssignments = Get-AzRoleAssignment
+
+  # Create an empty array to store user permissions
+  $userPermissions = @()
+
+  # Loop through each role assignment
+  foreach ($roleAssignment in $roleAssignments) {
+    # Check if the role assignment is for a user
+    if ($roleAssignment.PrincipalType -eq "User") {
+      # Create a hashtable to store the user's display name, email, and role definition name
+      $user = @{
+        DisplayName        = $roleAssignment.DisplayName
+        Email              = $roleAssignment.SignInName
+        RoleDefinitionName = $roleAssignment.RoleDefinitionName
+      }
+
+      # Add the user hashtable to the userPermissions array
+      $userPermissions += New-Object PSObject -Property $user
     }
   }
+
+  # Output the userPermissions array to a CSV file
+  $userPermissions | Export-Csv -Path "C:\UsersPermissions.csv" -Append -NoTypeInformation
 }
